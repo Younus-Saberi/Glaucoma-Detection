7bff18fff19903cfe530cfdc989ac7e1ebba36aa
Younus-Saberi <younussaberi@gmail.com>
2024-03-04 09:52:10 +0530

    test image processing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
 from typing import Union
 import shutil
 from pathlib import Path
@@ -61,7 +62,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 
         if not extensions:
             #TODO: convert image to suitable format 
-             return JSONResponse(content={"error": "Image should in jpg, jpeg or png format"}, status_code=400)
+            return JSONResponse(content={"error": "Image should in jpg, jpeg or png format"}, status_code=400)
         else:
             
             file_path = upload_dir / file.filename
@@ -76,7 +77,7 @@ async def create_upload_file(file: UploadFile = File(...)):
             uploaded_image = read_Imagefile(file.file.read())
 
 
-            print("===Read the Image===")
+            print("===Reading the Image===")
 
 
             print("===Doing Prediction")
@@ -90,4 +91,53 @@ async def create_upload_file(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
+@app.post("/api/image-processing")
+async def image_processing(file: UploadFile = File(...)):
+    try:
 
+        extensions = file.filename.split('.')[-1] in ("jpg","jpeg","png")
+
+        if not extensions:
+            #TODO: convert image to suitable format 
+            return JSONResponse(content={"error": "Image should in jpg, jpeg or png format"}, status_code=400)
+        else:
+            
+            '''file_path = 'uploaded_images\cirrus cloud.jpg'
+
+            with file_path.open('wb') as buffer:
+                shutil.copyfileobj(file.file, buffer)'''
+
+            print("===File Uploaded Successfully!!!===")
+
+                    # Read the uploaded image using the read_Imagefile function
+            '''file.file.seek(0)  # Reset the file pointer to the beginning
+            uploaded_image = read_Imagefile(file.file.read())'''
+            path = r'uploaded_images\\glaucoma.jpg'
+            res_path = r'temp\\glaucoma2.jpg'
+
+            process_image_ellipse(path)
+            print("function called")
+            # Return the prediction result as JSON response
+            return FileResponse(path=res_path, status_code=200)
+        
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+def process_image_ellipse(path:str):
+    #load image in cv2
+    img = cv2.imread(path,cv2.IMREAD_COLOR)
+
+    #define ellipse parameters
+    center = (100,100)
+    axes_length = (50,30)
+    angle = 30
+
+    #draw ellipse on image
+    color = (0,255,0)
+    thickness = 2
+    cv2.ellipse(img, center, axes_length, angle, 0,360,color, thickness)
+
+    cv2.imwrite('temp\glaucoma2.jpg', img)
+
+    
